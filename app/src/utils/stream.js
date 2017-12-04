f7af4af7aa5c39b4d2f11f45d63f81fd8de4039a
oliaboria <olgaboris94@gmai.com>
2017-12-05 01:33:56 +0300

    bundle css

--- app/src/utils/stream.js
+++ app/src/utils/stream.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const yargs = require('yargs');
 const path = require('path');
 const throught2 = require('through2');
-const { promisify } = require('util');
 const request = require('request');
-const {parseCsvToJson} = require('../common/helpers/helpers');
+const { parseCsvToJson } = require('../common/helpers/helpers');
 
 function inputOutput(filePath) {
     fs.createReadStream(filePath)
@@ -42,6 +41,32 @@ function convertCsvToJson() {
         const dataJson = parseCsvToJson(chunk.toString());
         callback(null, JSON.stringify(dataJson, null, ' '));
     });
+}
+
+function bundleCss(dir) {
+    const destinationFilename = 'bundle.css';
+    const bundle = fs.createWriteStream(`${dir}/${destinationFilename}`);
+
+    fs.readdir(dir, (err, files) => {
+        const streams = files
+            .filter(file => file !== destinationFilename) 
+            .map(file => path.join(dir, file))
+            .map(file => fs.createReadStream(file))
+            .concat(request('https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css'));
+
+        const write = (streams, dest) => {
+            const [ source, ...rest ] = streams;
+
+            if (source) {
+                source.pipe(bundle, { end: false });
+                source.on('end', () => {
+                    write(rest, dest);
+                });
+            }
+        };
+        
+        write(streams, bundle);
+    })
 }
 
 const argv = yargs
@@ -95,6 +120,19 @@ const argv = yargs
          },
          (argv) => parse(argv.file)
      )
+     .command(
+        'bundle-css',
+        'Bundles all css files in the folder into one file',
+        {
+            path: {
+                alias: 'p',
+                describe: 'directory with css files to bundle',
+                normalize: true,
+                demandOption: true,
+            }
+        },
+        (argv) => bundleCss(argv.path)
+    )
     .demandCommand(1, 'You need at least one command before moving on')
     .locale('en')
     .version(false)
