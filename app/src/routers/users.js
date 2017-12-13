5ba2da7b321af395a19fa2fa83284f0c81ed2dfb
oliaboria <olgaboris94@gmai.com>
2017-12-14 00:11:29 +0300

    drop users

--- app/src/routers/users.js
+++ app/src/routers/users.js
@@ -1,15 +1,30 @@
 import express from 'express';
 
 import jwt from '../middlewares/jwt';
-const users = [];
+import { User } from '../models/models';
 
 const router = express.Router();
 
 router.use(jwt);
 
 router.get('/', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(users, null, ' '));
+    User
+        .all()
+        .then((products) => res.status(200).send(products))
+        .catch((error) => res.status(400).send(error));
+});
+
+router.post('/', (req, res) => {
+    const user = {
+        "name": req.body.name,
+        "surname": req.body.surname,
+        "email": req.body.email,
+        "password": req.body.password
+    };
+    
+    User.create(user)
+        .then(user => res.status(201).send(user))
+        .catch(error => res.status(400).send(error));
 });
   
 export default router;
